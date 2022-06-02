6bef2697c9e560818485458f691713594cb43cce
Adam Jacobsson <adam.jacobsson@optidev.se>
2022-06-02 08:10:08 +0200

    search by ids

--- popup.js
+++ popup.js
@@ -17,39 +17,48 @@ document.getElementById('saveBtn').onclick = (e) => {
 document.getElementById('searchDateBtn').onclick = (e) => {
     var searchFrom = document.getElementById('searchDateInput').value;
     setOrDeleteLocalStorage('searchForDate', searchFrom);
+}
 
+const preloadData = (searchFrom) => {
+    var data = '';
     if (!searchFrom) {
+        alert('No searchFrom');
         return;
     }
-
+    
     chrome.storage.local.get(['wdid_obj'], function (result) {
-        var data = '';
         for (const [k, v] of Object.entries(result['wdid_obj'])) {
             if (v['date'] == searchFrom) {
-                data += k + '\n' + (v['value'] || '') + '\n\n';
+                if (!k || k == 'undefined') {
+                    //continue;
+                }
+                var table = document.getElementById("myTable");
+                var row = table.insertRow(0);
+                var cell1 = row.insertCell(0);
+                var cell2 = row.insertCell(1);
+                cell1.innerHTML = k;
+                cell2.innerHTML = (v['value'] || 'None').substring(0, 40);
             }
         }
 
         if (!data) {
-            window.confirm('Such empty');
+            document.getElementById('statsA').innerHTML = 'lul';
             return;
         }
-        window.confirm(data)
+        document.getElementById('statsA').innerHTML = data;
     });
 }
 
-try {
+window.onload = () => {
     chrome.storage.local.get(['lookForRe'], function (result) {
         document.getElementById('reInput').value = result['lookForRe'];
     });
     chrome.storage.local.get(['searchForDate'], function (result) {
         document.getElementById('searchDateInput').value = result['searchForDate'];
+        preloadData(result['searchForDate'])
     });
     chrome.storage.local.get(['lookForXPath'], function (result) {
         document.getElementById('xpathInput').value = result['lookForXPath'];
     });
+};
 
-}
-catch {
-
-}
